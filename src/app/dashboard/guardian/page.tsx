38440fae1365a314796f3a505a60dbb491814b2a
kawshan <kawshan.virantha9999@gmail.com>
2025-04-09 19:29:58 +0530

    finished dashboard

---
+++ src/app/dashboard/guardian/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div>
+
+            <div className="flex justify-center content-center h-[100px] p-2 m-2 rounded-2xl bg-green-600">
+                <div className="text-3xl p-5 font-bold">Guardian</div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Page;
