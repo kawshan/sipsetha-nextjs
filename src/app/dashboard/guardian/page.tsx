620fdb17af6bbe306dd7bcd2fcfcf250caab1ee9
kawshan <kawshan.virantha9999@gmail.com>
2025-04-14 11:28:42 +0530

    finished ui part

--- src/app/dashboard/guardian/page.tsx
+++ src/app/dashboard/guardian/page.tsx
@@ -11,6 +11,7 @@ import {
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label";
+import { Select,SelectContent,SelectGroup,SelectItem,SelectLabel,SelectTrigger,SelectValue,} from "@/components/ui/select"
 
 
 function Page() {
@@ -24,11 +25,16 @@ function Page() {
     const [address, setAddress] = useState("")
     const [guardiantype_id, setGuardianType] = useState(null)
     const [gender, setGender] = useState("")
-    const [user_id, setUserId] = useState(null)
-    const [note, setNote] = useState("")
-    const [addeddatetime, setAddeddatetime] = useState("")
-    const [modifydatetime, setModifydatetime] = useState("")
-    const [deletedatetime, setDeletedatetime] = useState("")
+    const [note, setNote] = useState("");
+
+
+    function handelGender (value:any):any{
+        setGender(value);
+        console.log(value);
+    }
+
+
+
 
 
     return (
@@ -81,15 +87,68 @@ function Page() {
 
                     <div className="grid grid-cols-4 gap-4">
 
-                        <div>
+                        <div className="col-span-1">
                             <Label className="text-lg font-bold">Land No <span className="text-blue-800">(Optional)</span>
                             </Label>
                             <Input type="text" value={landno} onChange={(e) => setLandNo(e.target.value)}></Input>
                         </div>
 
 
+                        <div className="col-span-3">
+                            <Label className="text-lg font-bold">Address</Label>
+                            <Input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} ></Input>
+                        </div>
 
                     </div>
+
+
+
+                    <div className="grid grid-cols-4 gap-4">
+
+
+                        <div className="col-span-1">
+                            <Label className="text-lg font-bold">Guardian Type</Label>
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select Guardian Type" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+
+
+
+                        <div className="col-span-1">
+                            <Label className="text-lg font-bold">Gender</Label>
+                            <Select onValueChange={handelGender}>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select Gender" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectItem value={"true"}>male</SelectItem>
+                                        <SelectItem value={"false"}>female</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+
+
+                        <div className="col-span-2">
+                            <Label className="text-lg font-bold">Note <span className="text-blue-800">(optional)</span> </Label>
+                            <Input type="text" value={note} onChange={(e) => setGender(e.target.value)}></Input>
+                        </div>
+
+                    </div>
+
+
+
 
 
                 </DialogContent>
