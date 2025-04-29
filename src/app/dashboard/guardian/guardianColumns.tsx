9a8cd7478e586bb50125f03bf16ebb2776b47f18
kawshan <kawshan.virantha9999@gmail.com>
2025-04-29 08:42:36 +0530

    added table actions

--- src/app/dashboard/guardian/guardianColumns.tsx
+++ src/app/dashboard/guardian/guardianColumns.tsx
@@ -3,6 +3,8 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown } from "lucide-react"
 import {Button} from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react"
+import {DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -65,4 +67,36 @@ export const columns: ColumnDef<Guardian>[] = [
         accessorKey: "status",
         header: "Status",
     },
+
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const guardian = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(String(guardian.id))}
+                        >
+                            Refill Guardian
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Delete  Guardian</DropdownMenuItem>
+                        <DropdownMenuItem>Print Guardian</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+
+
 ]
