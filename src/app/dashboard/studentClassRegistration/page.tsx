4e692b9b1b9fafcef0912f894a2648599a292b5d
kawshan <kawshan.virantha9999@gmail.com>
2025-08-11 09:41:22 +0530

    student class registration ui finished

--- src/app/dashboard/studentClassRegistration/page.tsx
+++ src/app/dashboard/studentClassRegistration/page.tsx
@@ -5,9 +5,9 @@ import {DataTable} from "@/components/data-table";
 import {
     getStudentClassRegistrationColumns
 } from "@/app/dashboard/studentClassRegistration/studentClassRegistrationColumns";
-
-
-
+import {TextGenerateEffect} from "@/components/ui/text-generate-effect";
+import {Label} from "@/components/ui/label";
+import {Button} from "@/components/ui/button";
 
 
 const Page = () => {
@@ -15,7 +15,7 @@ const Page = () => {
 
     const [id, setId] = useState("");
     const [indexNumber, setIndexNumber] = useState("");
-    const [fee,setFees] = useState("");
+    const [fee, setFees] = useState("");
     const [addeddatetime, setAddeddatetime] = useState("");
     const [deletedatetime, setDeletedatetime] = useState("");
     const [modifydatetime, setModifydatetime] = useState("");
@@ -23,57 +23,107 @@ const Page = () => {
     const [studentClassRegistrationList, setStudentClassRegistrationList] = useState([]);
 
 
-
-    useEffect(()=>{
+    useEffect(() => {
         getAllStudentClassRegistrations();
-    },[])
+    }, [])
+
+
+    const lblHeading = 'Student Class Registration Master';
 
 
 
-
-    const getAllStudentClassRegistrations = ()=>{
-        getAllStudentClassRegistrationService().then((res)=>{
+    const getAllStudentClassRegistrations = () => {
+        getAllStudentClassRegistrationService().then((res) => {
             setStudentClassRegistrationList(res.data);
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
             setStudentClassRegistrationList([]);
         })
     }
 
 
-
-    const refillStudentClassRegistration = ()=>{
-
-    }
-
-
-    const deleteStudentClassRegistration = ()=>{
+    const refillStudentClassRegistration = () => {
 
     }
 
 
-    const printStudentClassRegistration = ()=>{
+    const deleteStudentClassRegistration = () => {
 
     }
 
 
+    const printStudentClassRegistration = () => {
 
+    }
 
 
     return (
         <div>
 
+            <div className="text-center bg-slate-900 dark:bg-slate-900 p-6 rounded-full text-white dark:text-white mb-5">
+                <TextGenerateEffect words={lblHeading}/>
+            </div>
+
+            <div className="p-9">
+                    <div className="grid grid-cols-12 gap-4 mt-5">
+                        <div className="col-span-6 font-semibold">Select Student <i className="text-red-800">*</i> </div>
+                        <div className="col-span-6"></div>
+                    </div>
+
+
+                <div className="grid grid-cols-12 gap-4 mt-5">
+                    <div className="col-span-6 font-semibold">Select Class Offering <i className="text-red-800">*</i> </div>
+                </div>
 
 
 
+                <div className="grid grid-cols-12 gap-4 mt-5">
+                    <div className="col-span-6 font-semibold">Fee <i className="text-red-800">*</i> </div>
+                </div>
+
+
+                <div className="grid grid-cols-12 gap-4 mt-5">
+                    <div className="col-span-6 font-semibold">Registration Status</div>
+                </div>
+
+                <div className="grid grid-cols-12 gap-4 mt-5">
+                    <div className="col-span-6 font-semibold">Registration Type</div>
+                </div>
 
 
 
+                <div className="grid grid-cols-12 gap-4 mt-5">
+                    <div className="col-span-6 font-semibold">Note</div>
+                </div>
 
 
-        <div className="p-9">
-            <DataTable columns={getStudentClassRegistrationColumns(refillStudentClassRegistration,deleteStudentClassRegistration,printStudentClassRegistration)} data={studentClassRegistrationList} ></DataTable>
-        </div>
+                <div className="grid grid-cols-12 gap-4 mt-5">
+
+                    <div className="col-span-4">
+                        <Button variant="destructive">Reset</Button>
+                    </div>
+
+                    <div className="col-span-4 flex justify-center items-center">
+                        <Button variant="warning">Update</Button>
+                    </div>
+
+
+                    <div className="col-span-4 flex justify-end items-end">
+                        <Button variant="success">Save</Button>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+            <div className="p-9">
+                <DataTable
+                    columns={getStudentClassRegistrationColumns(refillStudentClassRegistration, deleteStudentClassRegistration, printStudentClassRegistration)}
+                    data={studentClassRegistrationList}></DataTable>
+            </div>
 
         </div>
     )
