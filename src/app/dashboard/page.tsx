11e6db497b752dc12ab6deec4873949dd2ce1aa9
kawshan <kawshan.virantha9999@gmail.com>
2025-08-29 09:01:49 +0530

    dashbord icons modifications

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -102,7 +102,7 @@ const Page = () => {
 
                 <div className="ms-10">
                     <Link href="/dashboard/classhall">
-                        <img src="/images/icons/guardian.png" alt="Guardian image" />
+                        <img src="/images/icons/classhall.png" alt="Class Hall image" />
                         <br/>
                         <p className="text-lg text-slate-700">Class Hall Master</p>
                     </Link>
@@ -110,7 +110,7 @@ const Page = () => {
 
                 <div>
                     <Link href="/dashboard/classRoomAllocation">
-                        <img src="/images/icons/student.png" alt="Student image"/>
+                        <img src="/images/icons/allocation.png" alt="allocation image"/>
                     </Link>
                     <br/>
                     <p className="text-lg text-slate-700">Class Room Allocation Master</p>
@@ -133,36 +133,36 @@ const Page = () => {
             <div className="grid grid-cols-5 gap-6 mt-10">
 
                 <div className="ms-10">
-                    <Link href="/dashboard/guardian">
-                        <img src="/images/icons/guardian.png" alt="Guardian image" />
+                    <Link href="/dashboard/teacher">
+                        <img src="/images/icons/teacher.png" alt="Teacher image" />
                         <br/>
-                        <p className="text-lg text-slate-700">Guardian Master</p>
+                        <p className="text-lg text-slate-700">Teacher Information Master</p>
                     </Link>
                 </div>
 
                 <div>
                     <Link href="/dashboard/student">
-                        <img src="/images/icons/student.png" alt="Student image"/>
+                        <img src="/images/icons/offering.png" alt="offering image"/>
                     </Link>
                     <br/>
-                    <p className="text-lg text-slate-700">Student Master</p>
+                    <p className="text-lg text-slate-700">Class Offering Master</p>
                 </div>
 
                 <div>
                     <Link href="/dashboard/studentClassRegistration">
-                        <img src="/images/icons/registration.png" alt="Registration image"/>
+                        <img src="/images/icons/enrollment.png" alt="enrollment image"/>
                     </Link>
                     <br/>
-                    <p className="text-lg text-slate-700">Student Class Registration Master</p>
+                    <p className="text-lg text-slate-700">Enrollment Master</p>
                 </div>
 
 
                 <div>
                     <Link href="/dashboard/payment">
-                        <img src="/images/icons/payment.png" alt="Payment image"/>
+                        <img src="/images/icons/teacherpayment.png" alt="Payment image for teacher"/>
                     </Link>
                     <br/>
-                    <p className="text-lg text-slate-700">Payment Master</p>
+                    <p className="text-lg text-slate-700">Teacher Payment Master</p>
                 </div>
 
 
